ba685bcb2338e409b2bb79173ac3d42a0aa84e84
PRAKHAR TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2023-06-16 18:50:52 +0530

    Update object.js

--- 16-6/object.js
+++ 16-6/object.js
@@ -3,7 +3,7 @@
 //     wheels: 50,
 //     engine: { cylinders: 4 , size: 2.2 },
 //   };
-// console.log(myHonda.engine.size)  
+// console.log(myHonda.engine.size)
 
 // const course={
 //     lectures:10,
@@ -15,10 +15,48 @@
 //     enroll(){
 //         console.log("you are enroll");
 //     }
-    
+
 // }
 // course.price=999;
 // course.enroll()
 // console.log(course)
 
- 
+//  const course = {
+//     tittle: "javascript"
+//  }
+//  const course_1 = {...course}
+//  course_1.tittle="java"
+//  console.log(course_1)
+
+// var family = {
+//   firstname: "ram",
+//   lastname: "kumar",
+//   age: 25,
+//   email: "ram@gmail.com",
+//   fullname: ["ram", "kumar", "dasrath"],
+//   livinig:{
+//      city:"ahmedabad",
+//      country:"india"
+//   }
+// };
+// console.log(family);
+// console.log(family.email);
+// console.log(family.fullname[0]);
+// console.log(family.livinig.city);
+
+// let person = {
+//     fname : "Prakhar",
+//     lname : "Tripathi",
+//     age: 17,
+//     hobbie:["criket","football","car","bike"]
+// };
+
+// person.age=18;
+// person.email="prakhartripati175.com";
+// delete person.age;
+
+// console.log("mname" in person);
+// console.log("mname" in person);
+// for(let key in person){
+//   console.log(key + ":" + person[key]);
+// }
