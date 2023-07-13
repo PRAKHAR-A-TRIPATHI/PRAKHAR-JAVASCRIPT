a2490500d25ecbf32946421155e93487ad8d7461
PRAKHAR TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2023-07-13 18:58:04 +0530

    7:00 update

---
+++ 13-7/dom-3/selector.js
@@ -0,0 +1,27 @@
+// let a = document.getElementById("p")
+// console.log(a);
+
+// let list  = document.querySelectorAll("li");
+// console.log(list);
+
+// list[1].style.color = "blue";
+
+// const myh1  = document.querySelectorAll("h1");
+// // console.log(myh1);
+// myh1.forEach(function(l){
+//     l.style.backgroundColor = "blue"
+// })
+
+//  cllass name 
+
+let clas = document.getElementsByClassName("list")
+// console.log(clas);
+// not work for each loop
+
+// let array = Array.from(clas);
+// console.log(array);
+
+// array.forEach(function(l){
+//     l.style.backgroundColor = "pink";
+// })
+
