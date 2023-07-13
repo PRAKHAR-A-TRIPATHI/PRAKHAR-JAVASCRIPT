6089cc3e3c88ee93b37f2c78dcbe82ad7d9d942f
PRAKHAR TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2023-07-13 17:23:43 +0530

    4:00 UPDATE

---
+++ 13-7/dom-2/accesingdomelement.js
@@ -0,0 +1,14 @@
+// var a =document.getElementById("html").innerHTML;
+// console.log(a);
+// document.write(a)
+
+// var b = document.getElementsByClassName("class")[2].innerHTML;
+// // console.log(b);
+
+// var tag = document.getElementsByTagName("p")
+// console.log(tag);
+
+// // accesing tag name,e 
+// var p = document.querySelector("body").innerHTML ;
+// console.log(p);
+
