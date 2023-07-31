7afc2fd1773687443d61df4f175d1948a5c36753
PRAKHAR TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2023-07-31 11:38:34 +0530

    11:00 am

---
+++ 31-7/new.js
@@ -0,0 +1,31 @@
+function isValidPassword(password) {
+    const specialCharacters = "!@#$%^&*()_+-=[]{};':\"\\|,.<>/?";
+    const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    let hasSpecialCharacter = false;
+    let hasUpperCase = false;
+    let hasLowerCase = false;
+    let hasNumber = false;
+    for (let i = 0; i < password.length; i++) {
+        if (specialCharacters.includes(password[i])) {
+            hasSpecialCharacter = true;
+        }
+        if (upperCaseLetters.includes(password[i])) {
+            hasUpperCase = true;
+        }
+        if (lowerCaseLetters.includes(password[i])) {
+            hasLowerCase = true;
+        }
+        if (numbers.includes(password[i])) {
+            hasNumber = true;
+        }
+    }
+    return hasSpecialCharacter && hasUpperCase && hasLowerCase && hasNumber;
+}
+const password = "myP@sswrd";
+if (isValidPassword(password)) {
+    console.log("Password is valid.");
+} else {
+    console.log("Password is not valid.");
+}
